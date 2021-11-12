e6965d7bd0e8a2b91eeb79ac441c3ba0f2a271c7
RijalArul <fakhrulmrijal7599@gmail.com>
2021-11-12 15:31:28 +0700

    Updated Login Accounts Error && Success

--- server/controllers/account.js
+++ server/controllers/account.js
@@ -1,3 +1,4 @@
+const { comparePassword } = require('../helpers/bcrypt')
 const { generateToken } = require('../helpers/jwt')
 const { Account } = require('../models')
 
@@ -12,7 +13,7 @@ class AccountController {
       const create = await Account.create(payload)
       if (create) {
         const access_token = generateToken({
-          userId: create.userId,
+          id: create.id,
           email: create.email
         })
 
@@ -26,6 +27,50 @@ class AccountController {
           return error.message
         })
         res.status(400).json(errors)
+      } else {
+        res.status(500).json({
+          msg: 'Internal Server Error'
+        })
+      }
+    }
+  }
+
+  static async login (req, res, next) {
+    try {
+      const loginData = {
+        email: req.body.email,
+        password: req.body.password
+      }
+
+      const user = await Account.findOne({
+        where: {
+          email: loginData.email
+        }
+      })
+
+      if (user) {
+        const matchPassword = comparePassword(loginData.password, user.password)
+
+        if (matchPassword) {
+          const access_token = generateToken({
+            id: user.id,
+            email: user.email
+          })
+
+          res.status(200).json({
+            access_token: access_token
+          })
+        } else {
+          throw { name: 'InvalidEmailOrPassword' }
+        }
+      } else {
+        throw { name: 'InvalidEmailOrPassword' }
+      }
+    } catch (err) {
+      if (err.name === 'InvalidEmailOrPassword') {
+        res.status(401).json({
+          msg: 'Invalid Password / Email'
+        })
       } else {
         res.status(500).json({
           msg: 'Internal Server Error'
--- server/routes/account.js
+++ server/routes/account.js
@@ -3,5 +3,6 @@ const AccountController = require('../controllers/account')
 const router = require('express').Router()
 
 router.post('/register', AccountController.register)
+router.post('/login', AccountController.login)
 
 module.exports = router
