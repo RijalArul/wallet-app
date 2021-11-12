7b107f29061d67d29cd5860dfac0fe0722a5651e
RijalArul <fakhrulmrijal7599@gmail.com>
2021-11-12 14:58:45 +0700

    Updated setup validation & error register accounts

--- server/controllers/account.js
+++ server/controllers/account.js
@@ -3,9 +3,20 @@ const { Account } = require('../models')
 class AccountController {
   static async register (req, res) {
     try {
-      console.log('ACCOUNT')
+      const { email, password, saldo } = req.body
+
+      const create = await Account.create(email, password, saldo)
     } catch (err) {
-      console.log(err)
+      if (err.name === 'SequelizeValidationError') {
+        const errors = err.errors.map(error => {
+          return error.message
+        })
+        res.status(400).json(errors)
+      } else {
+        res.status(500).json({
+          msg: 'Internal Server Error'
+        })
+      }
     }
   }
 }
--- server/models/account.js
+++ server/models/account.js
@@ -1,5 +1,6 @@
 'use strict'
 const { Model } = require('sequelize')
+const { hashPassword } = require('../helpers/bcrypt')
 module.exports = (sequelize, DataTypes) => {
   class Account extends Model {
     /**
@@ -24,13 +25,54 @@ module.exports = (sequelize, DataTypes) => {
   }
   Account.init(
     {
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
-      saldo: DataTypes.INTEGER
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'Email is required'
+          },
+          notEmpty: {
+            msg: 'Email is required'
+          },
+          isEmail: {
+            msg: 'Must be an email'
+          }
+        },
+        unique: {
+          args: true,
+          msg: 'Email address already exists'
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'Password is required'
+          },
+          notEmpty: {
+            msg: 'Password is required'
+          },
+          len: {
+            args: [5, 255],
+            msg: 'Password min 5 character'
+          }
+        }
+      },
+      saldo: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+      }
     },
     {
       sequelize,
-      modelName: 'Account'
+      modelName: 'Account',
+      hooks: {
+        beforeCreate (instance, options) {
+          instance.password = hashPassword(instance.password)
+        }
+      }
     }
   )
   return Account
